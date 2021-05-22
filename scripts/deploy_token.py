5486704f59170e0366706aedcbeea2e4cb8df267
Nikita Selin (Anodev) <farvard34@gmail.com>
2021-05-23 04:58:13 +1000

    Added tests

--- scripts/deploy_token.py
+++ scripts/deploy_token.py
@@ -1,5 +1,9 @@
-from brownie import CoffeeCoin, accounts
+from brownie import web3, CoffeeCoin, accounts
+import json
+import os
 
 def main():
-    acct = accounts.load('deploy_account')
+    priv_key = web3.eth.account.decrypt(
+        json.loads(os.environ["DEPLOY_ACCOUNT"]), os.environ["DEPLOY_ACCOUNT_PASSWORD"])
+    acct = accounts.add(private_key=priv_key)
     tx = acct.deploy(CoffeeCoin)
